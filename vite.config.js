767cc357bc5cbbb24baedd46f5fab0f59127e592
TheSamuelDopke <samueloficial1@hotmail.com>
2025-09-01 22:12:14 -0300

    second

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // Adicione a linha 'root' para dizer ao Vite para usar a pasta 'src'
+  root: 'src',
+  base: '/TrabalhoVite-01-09-25-PJP',
+
+})
